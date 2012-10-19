6de7beb266657fad47a73e5220b287a0d2a57cfe
Andres Abril <andres.abril@gmail.com>
2012-10-18 19:16:40 -0500

    -Se agregó el root view.

--- FuerzaAerea/RootViewController.m
+++ FuerzaAerea/RootViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2012 Andres Abril. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "RootViewController.h"
 
-@interface ViewController ()
+@interface RootViewController ()
 
 @end
 
-@implementation ViewController
+@implementation RootViewController
 
 - (void)viewDidLoad
 {
