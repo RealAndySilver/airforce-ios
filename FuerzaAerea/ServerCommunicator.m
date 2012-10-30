1157e65968b370f004107300c120650bb2a46eef
Andres Abril <andres.abril@gmail.com>
2012-10-30 12:52:52 -0500

    -Se agregó la clase ServerCommunicator encargada de hacer los requests y recibir las respuestas del server -Se agregó la clase XMLReader encargada de serializar los datos recibidos desde el servidor. Esta clase moldea el XML en un Diccionario legible.

---
+++ FuerzaAerea/ServerCommunicator.m
@@ -0,0 +1,106 @@
+//
+//  ServerCommunicator.m
+//  PrizeKing
+//
+//  Created by Andres Abril on 10/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ServerCommunicator.h"
+
+@implementation ServerCommunicator
+@synthesize dictionary,tag,caller,objectDic,resDic;
+-(id)init {
+    self = [super init];
+    if (self) {
+        tag = 0;
+        caller = nil;
+        webData = nil;
+        theConnection = nil;
+    }
+    return self;
+}
+-(void)callServerWithMethod:(NSString*)method 
+               andParameter:(NSString*)parameter{
+    NSString *soapMessage = [NSString stringWithFormat:
+                             @"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                             "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://droidsecure.dglabs.com/app/service/\"\n"
+                             "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"\n"
+                             "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
+                             "xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\">\n"
+                             "<soapenv:Header/>\n"
+                             "<soapenv:Body>\n"
+                             "<%@>\n"
+                             "%@\n"
+                             "</%@>\n"
+                             "</soapenv:Body>\n"
+                             "</soapenv:Envelope>\n",method,parameter,method];
+    tempMethod=method;
+	//NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.ekoomedia.com.co/ekoobot3d/web/ws/bot_api?wsdl"]];
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://droidsecure.dglabs.com/app/service/UserService.php"]];
+
+    NSString *soapAction=[NSString stringWithFormat:@"http://droidsecure.dglabs.com/app/service/"];
+	NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];  
+	NSString *msgLength = [NSString stringWithFormat:@"%d", [soapMessage length]];          
+	[theRequest addValue: @"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];       
+	[theRequest addValue: soapAction forHTTPHeaderField:@"SOAPAction"];
+	[theRequest addValue: msgLength forHTTPHeaderField:@"Content-Length"];
+	[theRequest setHTTPMethod:@"POST"];
+	[theRequest setHTTPBody: [soapMessage dataUsingEncoding:NSUTF8StringEncoding]];
+	theConnection = [[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+	dictionary = [[NSDictionary alloc]init];
+	if(theConnection) {
+		webData = [NSMutableData data];
+	}
+	else {
+		NSLog(@"theConnection is NULL");
+	}
+}
+
+
+//Implement the NSURL and XMLParser protocols
+#pragma mark -
+#pragma mark NSURLConnection methods
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response{
+	[webData setLength:0];
+	NSLog(@"didReceiveresponse");
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+	[webData appendData:data];
+	NSLog(@"didReceiveData");
+    
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error{
+	NSLog(@"didFailWithError");
+    if ([caller respondsToSelector:@selector(receivedDataFromServerWithError:)]) {
+        [caller performSelector:@selector(receivedDataFromServerWithError:) withObject:self];
+    }
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection{
+	NSString *methodResponse=[NSString stringWithFormat:@"ns1:%@Response",tempMethod];
+	NSLog(@"Todos los datos recibidos");
+    NSString *theXML = [[NSString alloc] initWithBytes:[webData mutableBytes] length:[webData length] encoding:NSUTF8StringEncoding];
+
+    NSDictionary *dictionary1 = [XMLReader dictionaryForXMLString:theXML error:nil];
+    
+    if ([caller respondsToSelector:@selector(receivedDataFromServer:)]) {
+        NSDictionary * dictionary2=[[[[dictionary1 objectForKey:@"SOAP-ENV:Envelope"]
+                                      objectForKey:@"SOAP-ENV:Body"]
+                                     objectForKey:methodResponse]
+                                    objectForKey:@"return"];
+        SBJSON *json=[[SBJSON alloc]init];
+        NSData *data=[[dictionary2 objectForKey:@"text"] dataUsingEncoding:NSUTF8StringEncoding];
+        NSString *json_string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSMutableDictionary *dit=[json objectWithString:json_string error:nil];
+        resDic=[[NSMutableDictionary alloc]initWithDictionary:dit];
+        NSLog(@"xml %@",resDic);
+        [caller performSelector:@selector(receivedDataFromServer:) withObject:self];
+    }
+
+}
+
+@end
---
+++ FuerzaAerea/XMLReader.h
@@ -0,0 +1,28 @@
+//
+//  XMLReader.h
+//  PrizeKing
+//
+//  Created by Andres Abril on 10/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMLReader : NSObject <NSXMLParserDelegate>
+{
+    NSMutableArray *dictionaryStack;
+    NSMutableString *textInProgress;
+    NSError *errorPointer;
+}
+
++ (NSDictionary *)dictionaryForPath:(NSString *)path error:(NSError **)errorPointer;
++ (NSDictionary *)dictionaryForXMLData:(NSData *)data error:(NSError **)errorPointer;
++ (NSDictionary *)dictionaryForXMLString:(NSString *)string error:(NSError **)errorPointer;
+
+@end
+
+@interface NSDictionary (XMLReaderNavigation)
+
+- (id)retrieveForPath:(NSString *)navPath;
+
+@end
